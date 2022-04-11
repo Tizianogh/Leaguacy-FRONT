b18eca7575ca43ec9915f7f73980974fabc756d4
Aïda Oukrif <aidaoukrif@gmail.com>
2022-04-11 22:47:02 +0200

    feat(init-front): Material ui+creation de modules+component+navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { TeamModule } from './modules/team/team.module';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    TeamModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/modules/team/team.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeamRoutingModule } from './team-routing.module';
+import { CreateTeamComponent } from './create-team/create-team.component';
+
+
+@NgModule({
+  declarations: [
+    CreateTeamComponent
+  ],
+  imports: [
+    CommonModule,
+    TeamRoutingModule
+  ]
+})
+export class TeamModule { }
